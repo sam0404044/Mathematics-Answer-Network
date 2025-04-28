dd44df1b5195a1e2a7814afcf99dfd665a44ae36
VincentChen <a978090@gmail.com>
2025-04-28 11:27:15 +0800

    globle css change

--- mathematice-answer-network/src/app/page.tsx
+++ mathematice-answer-network/src/app/page.tsx
@@ -1,37 +1,42 @@
-"use client";
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import styles from "./page.module.css";
-import HeroSection from "./components/HeroSection";
-import StartBtn from "./components/button/StartBtn";
-import About from "./components/About";
-import Menu from "./components/Menu";
-import Animation from "./components/Animation";
-import { useEffect, useState } from "react";
+'use client';
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+import styles from './page.module.css';
+import HeroSection from './components/HeroSection';
+import StartBtn from './components/button/StartBtn';
+import About from './components/About';
+import Menu from './components/Menu';
+import Animation from './components/Animation';
+import { useEffect, useState } from 'react';
 
 export default function page() {
-  const [isActive, setIsActive] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+    const [isActive, setIsActive] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(function () {
-    const fadeOutTimer = setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
+    useEffect(function () {
+        const fadeOutTimer = setTimeout(() => {
+            setIsLoading(false);
+        }, 5000);
 
-    return () => clearTimeout(fadeOutTimer);
-  }, []);
+        return () => clearTimeout(fadeOutTimer);
+    }, []);
 
-  return (
-    <div className={styles.container}>
-      {isLoading ? <Animation /> : ""}
-      <NavBar isActive={isActive} onIsActive={setIsActive} />
-      {isActive ? <Menu onIsActive={setIsActive} /> : ""}
-      <div className={styles.content}>
-        <HeroSection />
-        <StartBtn>Start answering</StartBtn>
-        <About />
-      </div>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            {isLoading ? (
+                <Animation />
+            ) : (
+                <>
+                    <NavBar isActive={isActive} onIsActive={setIsActive} />
+                    {isActive ? <Menu onIsActive={setIsActive} /> : ''}
+                    <div className={styles.content}>
+                        <HeroSection />
+                        <StartBtn>Start answering</StartBtn>
+                        <About />
+                    </div>
+                    <Footer />
+                </>
+            )}
+        </div>
+    );
 }
