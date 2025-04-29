e649a16145bbd06cb6cdd6a42db4fb4448012da0
vincentchen <a978090@gmail.com>
2025-04-29 20:51:57 +0800

    完成登入系統

--- mathematice-answer-network/src/lib/db.ts
+++ mathematice-answer-network/src/lib/db.ts
@@ -1,6 +1,7 @@
 // src/lib/db.ts
 import mysql from 'mysql2/promise';
 
+// 抓取.env.local中的內容並設定為連線參數
 const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT),
@@ -9,6 +10,4 @@ const pool = mysql.createPool({
     database: process.env.DATABASE_NAME,
 });
 
-// console.log('DB HOST:', process.env.DATABASE_HOST);
-// console.log('DB USER:', process.env.DATABASE_USER);
 export default pool;
