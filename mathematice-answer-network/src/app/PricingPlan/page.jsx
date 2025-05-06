b9907fef64dedd11129eb37a3516aad8906dc3d3
VincentChen <a978090@gmail.com>
2025-05-06 11:59:27 +0800

    合併分支、修改顏色

--- mathematice-answer-network/src/app/PricingPlan/page.jsx
+++ mathematice-answer-network/src/app/PricingPlan/page.jsx
@@ -6,94 +6,88 @@ import NavBar from "../components/NavBar";
 import Menu from "../components/Menu";
 
 export default function PricingPage() {
-  const [isActive, setIsActive] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+    const [isActive, setIsActive] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
-  return (
-    <>
-      <NavBar isActive={isActive} onIsActive={setIsActive} />
-      {isActive ? <Menu onIsActive={setIsActive} /> : ""}
+    return (
+        <>
+            <NavBar isActive={isActive} onIsActive={setIsActive} />
+            {isActive ? <Menu onIsActive={setIsActive} /> : ""}
 
-      <div className="p-[10px] min-h-screen">
-        <h2 className=" text-center text-[18px] mb-[15px]  text-[#333] font-pricing">
-          Pricing
-        </h2>
+            <div className="p-[10px] min-h-screen bg-(--background)">
+                <h2 className=" text-center text-[18px] mb-[15px]  text-[#333] font-pricing">
+                    Pricing
+                </h2>
 
-        {/* 免費方案 */}
-        <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
-          <div className="font-bold text-[18px] mt-[10px]">
-            ✅ 免費方案（Free）
-          </div>
-          <div className="text-[12px] text-[#666] mb-[10px] ">開始嘗試答題</div>
-          <div className="font-bold text-[16px] mb-[10px]">$ 0/month</div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制進行隨機答題
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制回答歷史題庫
-          </div>
-          <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
-            開始體驗
-          </button>
-        </div>
+                {/* 免費方案 */}
+                <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
+                    <div className="font-bold text-[18px] mt-[10px]">✅ 免費方案（Free）</div>
+                    <div className="text-[12px] text-[#666] mb-[10px] ">開始嘗試答題</div>
+                    <div className="font-bold text-[16px] mb-[10px]">$ 0/month</div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制進行隨機答題
+                    </div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制回答歷史題庫
+                    </div>
+                    <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
+                        開始體驗
+                    </button>
+                </div>
 
-        {/* 月費方案 */}
-        <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
-          <div className="font-bold text-[18px] mt-[10px]">
-            💎 月費方案（Subscription）
-          </div>
-          <div className="text-[12px] text-[#666] mb-[10px]">
-            無限練習，持續進步
-          </div>
-          <div className="font-bold text-[16px] mb-[10px]">NT$99 / 月</div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制進行隨機答題
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制回顧歷史題庫
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            享有進階解答分析輔助
-          </div>
-          <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
-            立即訂閱
-          </button>
-        </div>
+                {/* 月費方案 */}
+                <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
+                    <div className="font-bold text-[18px] mt-[10px]">
+                        💎 月費方案（Subscription）
+                    </div>
+                    <div className="text-[12px] text-[#666] mb-[10px]">無限練習，持續進步</div>
+                    <div className="font-bold text-[16px] mb-[10px]">NT$99 / 月</div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制進行隨機答題
+                    </div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制回顧歷史題庫
+                    </div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        享有進階解答分析輔助
+                    </div>
+                    <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
+                        立即訂閱
+                    </button>
+                </div>
 
-        {/* 點數儲值方案 */}
-        <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
-          <div className="font-bold text-[18px] mt-[10px]">
-            💰 點數儲值方案（Credits）
-          </div>
-          <div className="text-[12px] text-[#666] mb-[10px]">
-            彈性儲值，自由掌握答題節奏
-          </div>
-          <div className="font-bold text-[16px] mb-[10px]">
-            NT$500 / 5000 題詳細解答
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制進行隨機答題
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            無限制回顧歷史題庫
-          </div>
-          <div className="flex items-center justify-center mb-[15px] text-[12px] text-(--plan-text-list)">
-            <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
-            每題詳細解答扣除 1 點，點數永久有效。
-          </div>
-          <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
-            儲值點數
-          </button>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+                {/* 點數儲值方案 */}
+                <div className=" p-[10px] m-[10px] bg-[var(--white)] rounded-[12px] text-center font-pricing text-[#333]">
+                    <div className="font-bold text-[18px] mt-[10px]">
+                        💰 點數儲值方案（Credits）
+                    </div>
+                    <div className="text-[12px] text-[#666] mb-[10px]">
+                        彈性儲值，自由掌握答題節奏
+                    </div>
+                    <div className="font-bold text-[16px] mb-[10px]">NT$500 / 5000 題詳細解答</div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制進行隨機答題
+                    </div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px]  text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        無限制回顧歷史題庫
+                    </div>
+                    <div className="flex items-center justify-center mb-[15px] text-[12px] text-(--plan-text-list)">
+                        <span className="h-[10px] w-[10px] bg-(--plan-list) rounded-[50%] inline-block mr-[8px]"></span>
+                        每題詳細解答扣除 1 點，點數永久有效。
+                    </div>
+                    <button className="bg-[var(--plan-subscribe-btn)] text-(--white) py-[6px] px-[20px] border-none rounded-[20px] cursor-pointer text-[14px]">
+                        儲值點數
+                    </button>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
 }
--- mathematice-answer-network/src/app/login/page.tsx
+++ mathematice-answer-network/src/app/login/page.tsx
@@ -6,173 +6,147 @@ import Notice from "../components/Notice";
 import Footer from "../components/Footer";
 
 export default function Login() {
-  const [showModal, setShowModal] = useState(false);
-  const [message, setMessage] = useState("");
-  const [userInfo, setUserInfo] = useState({
-    email: "",
-    password: "",
-    rememberMe: false,
-  });
-
-  // 登入按鈕
-  const login = async () => {
-    // console.log(userInfo);
-    // 傳送資料給後端
-    const res = await fetch("/api/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(userInfo),
+    const [showModal, setShowModal] = useState(false);
+    const [message, setMessage] = useState("");
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        rememberMe: false,
     });
 
-    // 接收後端回傳的資料，!res.ok代表拋出錯誤訊息
-    // res.json(): 取得後端回傳的資料
-    if (!res.ok) {
-      // const err = await res.json();
-      const err = await res.json();
-      console.log(err);
-      setMessage(err.message || "伺服器錯誤");
-      setShowModal(true);
-      console.log(err);
-      return;
-    } else {
-      setMessage("登入成功");
-      setShowModal(true);
-      return;
-    }
-  };
+    // 登入按鈕
+    const login = async () => {
+        // console.log(userInfo);
+        // 傳送資料給後端
+        const res = await fetch("/api/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(userInfo),
+        });
 
-  return (
-    <>
-      <main className="relative flex flex-col items-center bg-[var(--background)] h-full pt-10 px-[40px] min-h-screen">
-        {/* 關閉按鈕 */}
-        <button className="absolute top-5 right-5">
-          <Link href={"/"}>
-            <Image
-              src={"/img/close.svg"}
-              alt="LoginImg"
-              width={30}
-              height={30}
-            ></Image>
-          </Link>
-        </button>
+        // 接收後端回傳的資料，!res.ok代表拋出錯誤訊息
+        // res.json(): 取得後端回傳的資料
+        if (!res.ok) {
+            // const err = await res.json();
+            const err = await res.json();
+            console.log(err);
+            setMessage(err.message || "伺服器錯誤");
+            setShowModal(true);
+            console.log(err);
+            return;
+        } else {
+            setMessage("登入成功");
+            setShowModal(true);
+            return;
+        }
+    };
 
-        {/* 插圖 */}
-        <Image
-          src={"/img/LoginImg.svg"}
-          alt="LoginImg"
-          width={372}
-          height={283}
-        ></Image>
+    return (
+        <>
+            <main className="relative flex flex-col items-center bg-[var(--background)] h-full pt-10 px-[40px] min-h-screen">
+                {/* 關閉按鈕 */}
+                <button className="absolute top-5 right-5">
+                    <Link href={"/"}>
+                        <Image src={"/img/close.svg"} alt="LoginImg" width={30} height={30}></Image>
+                    </Link>
+                </button>
 
-        {/* 標題 */}
-        <h1 className="text-4xl text-center font-bold py-5">
-          Wellcome to MWBB!
-        </h1>
+                {/* 插圖 */}
+                <Image src={"/img/LoginImg.svg"} alt="LoginImg" width={372} height={283}></Image>
 
-        {/* 登入表單 */}
-        <div className="w-full max-w-[372px] mx-auto">
-          <form className="bg-[var(--form-background)] rounded-xl w-full  space-y-4 p-5">
-            {/* email */}
-            <div>
-              <label
-                htmlFor=""
-                className="block text-sm text-[var(--header-text)] mb-2 font-bold"
-              >
-                電子郵件:
-              </label>
+                {/* 標題 */}
+                <h1 className="text-4xl text-center font-bold py-5">Wellcome to MWBB!</h1>
 
-              <input
-                type="text"
-                className="w-full  focus:ring-2 focus:ring-[var(--secondColor)] border border-[var(--secondColor)] rounded-lg focus:outline-none py-1 px-1"
-                value={userInfo.email}
-                onChange={(e) =>
-                  setUserInfo({ ...userInfo, email: e.target.value })
-                }
-              />
-            </div>
-            {/* password */}
-            <div>
-              <label
-                htmlFor=""
-                className="block text-sm text-[var(--subtitleColor)] mb-2 font-bold"
-              >
-                密碼:
-              </label>
-              <input
-                type="password"
-                className="w-full  focus:ring-2 focus:ring-[var(--header-text)] border border-[var(--header-text)] rounded-lg focus:outline-none py-1 px-1"
-                value={userInfo.password}
-                onChange={(e) =>
-                  setUserInfo({ ...userInfo, password: e.target.value })
-                }
-              />
-            </div>
-            {/* 記住我、忘記密碼? */}
-            <div className="flex items-center justify-between text-sm ">
-              <label className="flex items-center space-x-2">
-                <input
-                  type="checkbox"
-                  className="w-4 h-4 border-[var(--header-text)]"
-                  checked={userInfo.rememberMe}
-                  onChange={(e) =>
-                    setUserInfo({ ...userInfo, rememberMe: e.target.checked })
-                  }
-                />
-                <span className="text-[var(--header-text)] font-bold">
-                  記住我
-                </span>
-              </label>
-              <Link href="/forgot-password" className="text-[var(--subtitle)]">
-                忘記密碼?
-              </Link>
-            </div>
-            <button
-              className="w-full bg-[var(--log-in-btn)] text-white py-2 rounded-lg font-bold"
-              onClick={login}
-              type="button"
-            >
-              登入
-            </button>
-            <Link
-              href={"/api/auth/google"}
-              className="w-full bg-[var(--log-in-btn)] text-[var(--text-btn)] py-2 rounded-lg font-bold flex items-center justify-center space-x-2"
-            >
-              <Image
-                src="/img/google.svg"
-                alt="Google"
-                width={20}
-                height={20}
-              />
-              <span>以Google登入</span>
-            </Link>
-          </form>
-        </div>
+                {/* 登入表單 */}
+                <div className="w-full max-w-[372px] mx-auto">
+                    <form className="bg-(--white) rounded-xl w-full  space-y-4 p-5">
+                        {/* email */}
+                        <div>
+                            <label
+                                htmlFor=""
+                                className="block text-sm text-[var(--header-text)] mb-2 font-bold"
+                            >
+                                電子郵件:
+                            </label>
 
-        {/* 註冊? */}
-        <div className="max-w-[372px] mx-auto w-full">
-          <div className="flex items-center justify-between my-5">
-            <label htmlFor="" className="text-[var(--subtitle)]">
-              還沒有帳號?
-            </label>
-            <Link
-              href={"/register"}
-              className="font-bold text-[var(--header-text)]"
-            >
-              註冊
-            </Link>
-          </div>
-        </div>
+                            <input
+                                type="text"
+                                className="w-full  focus:ring-2 focus:ring-[var(--secondColor)] border border-[var(--secondColor)] rounded-lg focus:outline-none py-1 px-1"
+                                value={userInfo.email}
+                                onChange={(e) =>
+                                    setUserInfo({ ...userInfo, email: e.target.value })
+                                }
+                            />
+                        </div>
+                        {/* password */}
+                        <div>
+                            <label
+                                htmlFor=""
+                                className="block text-sm text-[var(--subtitleColor)] mb-2 font-bold"
+                            >
+                                密碼:
+                            </label>
+                            <input
+                                type="password"
+                                className="w-full  focus:ring-2 focus:ring-[var(--header-text)] border border-[var(--header-text)] rounded-lg focus:outline-none py-1 px-1"
+                                value={userInfo.password}
+                                onChange={(e) =>
+                                    setUserInfo({ ...userInfo, password: e.target.value })
+                                }
+                            />
+                        </div>
+                        {/* 記住我、忘記密碼? */}
+                        <div className="flex items-center justify-between text-sm ">
+                            <label className="flex items-center space-x-2">
+                                <input
+                                    type="checkbox"
+                                    className="w-4 h-4 border-[var(--header-text)]"
+                                    checked={userInfo.rememberMe}
+                                    onChange={(e) =>
+                                        setUserInfo({ ...userInfo, rememberMe: e.target.checked })
+                                    }
+                                />
+                                <span className="text-[var(--header-text)] font-bold">記住我</span>
+                            </label>
+                            <Link href="/forgot-password" className="text-[var(--subtitle)]">
+                                忘記密碼?
+                            </Link>
+                        </div>
+                        <button
+                            className="w-full bg-[var(--log-in-btn)] text-white py-2 rounded-lg font-bold"
+                            onClick={login}
+                            type="button"
+                        >
+                            登入
+                        </button>
+                        <Link
+                            href={"/api/auth/google"}
+                            className="w-full bg-[var(--log-in-btn)] text-[var(--text-btn)] py-2 rounded-lg font-bold flex items-center justify-center space-x-2"
+                        >
+                            <Image src="/img/google.svg" alt="Google" width={20} height={20} />
+                            <span>以Google登入</span>
+                        </Link>
+                    </form>
+                </div>
 
-        {/* 彈窗 */}
-        {/* 有條件渲染的彈窗 */}
-        <Notice
-          show={showModal}
-          onClose={() => setShowModal(false)}
-          message={message}
-        />
-      </main>
-      {/* footer */}
-      <Footer />
-    </>
-  );
+                {/* 註冊? */}
+                <div className="max-w-[372px] mx-auto w-full">
+                    <div className="flex items-center justify-between my-5">
+                        <label htmlFor="" className="text-[var(--subtitle)]">
+                            還沒有帳號?
+                        </label>
+                        <Link href={"/register"} className="font-bold text-[var(--header-text)]">
+                            註冊
+                        </Link>
+                    </div>
+                </div>
+
+                {/* 彈窗 */}
+                {/* 有條件渲染的彈窗 */}
+                <Notice show={showModal} onClose={() => setShowModal(false)} message={message} />
+            </main>
+            {/* footer */}
+            <Footer />
+        </>
+    );
 }
