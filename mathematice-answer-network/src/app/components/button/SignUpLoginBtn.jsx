faec8974d53e1f5f97a278d8634d2a2a6737f59b
luo0715 <a0975140151j@gmail>
2025-05-16 14:33:05 +0800

    logout fix

--- mathematice-answer-network/src/app/components/button/SignUpLoginBtn.jsx
+++ mathematice-answer-network/src/app/components/button/SignUpLoginBtn.jsx
@@ -1,14 +1,7 @@
 import Link from "next/link";
 import styles from "./SignUpLoginBtn.module.css";
 
-export default function SignUpLogin({
-  children,
-  isActive,
-  type,
-  route = "/",
-  setIsLogin,
-  isLogin,
-}) {
+export default function SignUpLogin({ children, isActive, type, route = "/", setIsLogin, isLogin }) {
   const logout = async function () {
     const res = await fetch("/api/logout");
     if (res.ok) {
@@ -18,9 +11,9 @@ export default function SignUpLogin({
 
   if (isActive) return null;
   return isLogin ? (
-    <button className={styles[type]} onClick={logout}>
-      {children}
-    </button>
+    <Link href={route} onClick={logout}>
+      <button className={styles[type]}>{children}</button>
+    </Link>
   ) : (
     <Link href={route}>
       <button className={styles[type]}>{children}</button>
